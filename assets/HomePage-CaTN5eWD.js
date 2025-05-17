2a2a2f16eb58833f6466421625b1cb6ec5c54834
IhorYarema <u96ok69@gmail.com>
2025-05-17 17:32:29 +0000

    Deploying to gh-pages from @ IhorYarema/goit-react-hw-05@d9956b06091ea9610fcac53a79608ea0359d4231 🚀

---
+++ assets/HomePage-CaTN5eWD.js
@@ -0,0 +1,2 @@
+import{r as s,g as o,j as e}from"./index-B_OqbBSr.js";import{M as r}from"./MovieList-D-6lCdmu.js";function t(){const[a,i]=s.useState([]);return s.useEffect(()=>{o().then(i)},[]),e.jsxs("div",{className:t.page,children:[e.jsx("h1",{className:t.heading,children:"Trending Today"}),e.jsx(r,{movies:a})]})}export{t as default};
+//# sourceMappingURL=HomePage-CaTN5eWD.js.map
