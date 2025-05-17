60c71798e1f2fb58ecdf7f8be4db16cc945012eb
IhorYarema <u96ok69@gmail.com>
2025-05-17 13:04:24 +0000

    Deploying to gh-pages from @ IhorYarema/goit-react-hw-05@9cb7884081dbd470acbd2ecde4eb1d24d661980d 🚀

---
+++ assets/HomePage-CqHHjxhM.js
@@ -0,0 +1,2 @@
+import{r as s,g as i,j as e}from"./index-DIP5Wvxs.js";import{M as r}from"./MovieList-C7j9x-wt.js";function t(){const[o,a]=s.useState([]);return s.useEffect(()=>{i().then(a)},[]),e.jsxs("div",{className:t.homePage,children:[e.jsx("h1",{className:t.title,children:"Trending Today"}),e.jsx(r,{movies:o})]})}export{t as default};
+//# sourceMappingURL=HomePage-CqHHjxhM.js.map
---
+++ assets/NotFoundPage-B-RED3Lm.js
@@ -0,0 +1,2 @@
+import{j as o,L as t}from"./index-DIP5Wvxs.js";const s={};function n(){return o.jsxs("div",{className:s.notFound,children:[o.jsx("h2",{className:s.title,children:"Page not found"}),o.jsx(t,{to:"/",children:"Go to Home"})]})}export{n as default};
+//# sourceMappingURL=NotFoundPage-B-RED3Lm.js.map
